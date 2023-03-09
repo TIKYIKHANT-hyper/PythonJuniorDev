f9e9f1a50b5d87f7b3a59842cecf22abbeac57e3
Default_String <tikyikhant2334@gmail.com>
2023-03-09 23:55:48 +0630

    linked lists added

--- linked_list.py
+++ linked_list.py
@@ -6,3 +6,19 @@ class node:
 
     def __repr__(self):
         return "<Node data: %s>" %self.data
+
+class linked_lists:
+    def __init__(self):
+        self.header = None
+
+    def isEmpty(self):
+        return self.header == None
+
+    def size(self):
+        count = 0
+        current = self.header
+        while current != None:
+            count += 1
+            current = current.next_node
+
+        return count
