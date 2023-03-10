8860326ac80e7c3c46b747031e82ea684ef88255
Default_String <tikyikhant2334@gmail.com>
2023-03-10 13:13:51 +0630

    linkedlists

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-def bubblesort_once(l):
-    newlist = list(l)
-    for i in range(0,len(l)-1,1):
-        if newlist[i] > newlist[i+1]:
-            tempdata = newlist[i]
-            newlist[i] = newlist[i+1]
-            newlist[i+1] = tempdata
-    return newlist
+class node:
+    def __init__(self, id, next):
+        self.id = id
+        self.next = next
 
+    def __repr__(self):
+        return f"Object code as {self.id} indicates {self.next}"
+class structure:
+    def __init__(self):
+        self.header = None
 
+    def size(self):
+        count = 0
+        while self.header != None:
+            count += 1
+        return count
+    def adder(self,data):
+        new_node = node(data,self.header)
+        self.header = new_node
 
-test1 = [9, 7, 5, 3, 1, 2, 4, 6, 8]
-length = len(test1)
-result = bubblesort_once(test1)
-print(length)
-print(result)
+testcase1 = structure()
+data1 = node(1,None)
+testcase1.header = data1
+data2 = node(2,None)
+testcase1.adder(data2)
+print(data1)
+print(testcase1)
