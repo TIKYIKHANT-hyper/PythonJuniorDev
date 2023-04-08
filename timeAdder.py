3d0b236896c63da411e0612eebc2353b266078fd
Default_String <tikyikhant2334@gmail.com>
2023-04-08 13:53:10 +0630

    timeAdder freecodecamp

--- timeAdder.py
+++ timeAdder.py
@@ -1,22 +1,13 @@
-"""
-def timeAdd(start,end,date):
-    days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
-    newlist = start.split(":")
-    res_period = ""
-    if int(newlist[0]) > 12:
-        if "AM" in start:
-            res_period += "PM"
-        elif "am" in start:
-            res_period += "pm"
-        elif "PM" in start:
-            res_period += "AM"
-        elif "pm" in start:
-            res_period += "pm"
-        else:
-            int(end) - int(newlist[0])
-"""
+def timeAdd(start,end,day=""):
+    date_count = 0
+    if day != "" and day != None:
+        days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        for date in days:
+            if date.lower() == day.lower():
+                break
+            else:
+                date_count += 1
 
-def timeAdd(start,end,day=None):
     if "AM" not in start.upper() and "PM" not in start.upper():
         time1 = start.strip()
         time2 = end.strip()
@@ -24,10 +15,10 @@ def timeAdd(start,end,day=None):
         newtime2 = time2.split(":")
         res_hr = int(newtime1[0]) + int(newtime2[0])
         res_min = int(newtime1[1]) + int(newtime2[1])
-        if res_min >= 60:
+        while res_min >= 60:
             res_hr += 1
             res_min -= 60
-        if res_hr > 12:
+        while res_hr > 12:
             res_hr -= 12
         print(f"{res_hr}:{res_min}\n")
     elif "AM" in start.upper() or "PM" in start.upper():
@@ -37,20 +28,39 @@ def timeAdd(start,end,day=None):
         end_str = (end.strip()).split(":")
         res_min = int(new_str[1]) + int(end_str[1])
         res_hr = int(new_str[0]) + int(end_str[0])
-        if res_min >= 60:
+
+        while res_min >= 60:
             res_hr += 1
             res_min -= 60
-        if res_hr > 12:
+        while res_hr > 12:
             res_hr -= 12
+
             if period == "pm":
-                print(f"{res_hr}:{res_min} am\n")
-            elif period == "PM":
-                print(f"{res_hr}:{res_min} AM\n")
+                period = "am"
+                if day != "" and day != None:
+                    if date_count == len(days):
+                        date_count = 0
+                    elif date_count < len(days):
+                        date_count += 1
+                        if date_count == len(days):
+                            date_count = 0
             elif period == "am":
-                print(f"{res_hr}:{res_min} pm\n")
+                period = "pm"
+            elif period == "PM":
+                period = "AM"
+                if day != "" and day != None:
+                    if date_count == len(days):
+                        date_count = 0
+                    elif date_count < len(days):
+                        date_count += 1
+                        if date_count == len(days):
+                            date_count = 0
             elif period == "AM":
-                print(f"{res_hr}:{res_min} PM\n")
+                period = "PM"
+
+        if day != None and day != "":
+            print(f"{res_hr}:{res_min} {period} {days[date_count]}")
         else:
             print(f"{res_hr}:{res_min} {period}\n")
 
-timeAdd("10:30 PM","2:20")
+timeAdd("12:30 PM","50:40","SunDay")
