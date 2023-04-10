2702065cbaff3255cd482ea8f9e18e7dc3cdfc2a
Default_String <tikyikhant2334@gmail.com>
2023-04-10 22:55:32 +0630

    final solution with all tests passed

--- timeAdder.py
+++ timeAdder.py
@@ -2,8 +2,19 @@ def add_time(start,duration,day=None):
     new_time = ""
     date_count = 0
     day_passed = 0
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    twentyfourhour_division_check = (duration.strip()).split(":")
+    hour_check = int(twentyfourhour_division_check[0])
+    minute_check = int(twentyfourhour_division_check[1])
+    start_str = (start.strip()).split(":")
+
+    start_min_section = (start_str[1]).split(" ")
+    start_min_check = int(start_min_section[0])
+    if (hour_check % 24) == 0 and (minute_check + start_min_check) < 60:
+        day_pass_by_hour_check = hour_check // 24
+        day_passed += day_pass_by_hour_check
     if day != "" and day != None:
-        days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
         for date in days:
             if date.lower() == day.lower():
                 break
@@ -30,7 +41,7 @@ def add_time(start,duration,day=None):
         """
         if res_min_len == 1:
             res_min = f"0{res_min}"
-        new_time = f"{res_hr}:{res_min}\n"
+        new_time = f"{res_hr}:{res_min}"
     elif "AM" in start.upper() or "PM" in start.upper():
         new_list = (start.strip()).split(" ")
         new_str = new_list[0].split(":")
@@ -42,45 +53,50 @@ def add_time(start,duration,day=None):
         while res_min >= 60:
             res_hr += 1
             res_min -= 60
+
         while res_hr > 12:
             res_hr -= 12
 
             if period == "pm":
                 period = "am"
-                if day != "" and day != None:
+
+                if date_count == len(days):
+                    date_count = 0
+                    if (hour_check % 24) == 0 and (minute_check + start_min_check) < 60:
+                        continue
+                    else:
+                        day_passed += 1
+                elif date_count < len(days):
+                    date_count += 1
+                    if (hour_check % 24) == 0 and (minute_check + start_min_check) < 60:
+                        continue
+                    else:
+                        day_passed += 1
                     if date_count == len(days):
                         date_count = 0
-                        day_passed += 1
-                    elif date_count < len(days):
-                        date_count += 1
-                        day_passed += 1
-                        if date_count == len(days):
-                            date_count = 0
             elif period == "am":
                 period = "pm"
             elif period == "PM":
                 period = "AM"
-                if day != "" and day != None:
+                if date_count == len(days):
+                    date_count = 0
+                    if (hour_check % 24) == 0 and (minute_check + start_min_check) < 60:
+                        continue
+                    else:
+                        day_passed += 1
+                elif date_count < len(days):
+                    date_count += 1
+                    if (hour_check % 24) == 0 and (minute_check + start_min_check) < 60:
+                        continue
+                    else:
+                        day_passed += 1
                     if date_count == len(days):
                         date_count = 0
-                        day_passed += 1
-                    elif date_count < len(days):
-                        date_count += 1
-                        day_passed += 1
-                        if date_count == len(days):
-                            date_count = 0
             elif period == "AM":
                 period = "PM"
         if res_hr == 12 and period.upper() == "AM":
             period = "PM"
-            if date_count == len(days):
-                date_count = 0
-                day_passed += 1
-            elif date_count < len(days):
-                date_count += 1
-                day_passed += 1
-                if date_count == len(days):
-                    date_count = 0
+
         elif res_hr == 12 and period.upper() == "PM":
             period = "AM"
             if date_count == len(days):
@@ -101,14 +117,19 @@ def add_time(start,duration,day=None):
             res_min = f"0{res_min}"
         if day != None and day != "":
             if day_passed == 0:
-                new_time = f"{res_hr}:{res_min} {period} {days[date_count]}"
+                new_time = f"{res_hr}:{res_min} {period}, {days[date_count]}"
             elif day_passed == 1:
-                new_time = f"{res_hr}:{res_min} {period} {days[date_count]} (next day)\n"
+                new_time = f"{res_hr}:{res_min} {period}, {days[date_count]} (next day)"
             else:
-                new_time = f"{res_hr}:{res_min} {period} {days[date_count]} ({day_passed} days later)\n"
+                new_time = f"{res_hr}:{res_min} {period}, {days[date_count]} ({day_passed} days later)"
+        elif (day == None or day == "") and day_passed > 0:
+            if day_passed == 1:
+                new_time = f"{res_hr}:{res_min} {period} (next day)"
+            else:
+                new_time = f"{res_hr}:{res_min} {period} ({day_passed} days later)"
         else:
-            new_time = f"{res_hr}:{res_min} {period}\n"
+            new_time = f"{res_hr}:{res_min} {period}"
     return new_time
 
-test1 = add_time("11:59 AM","24:05","saturDay")
-print(add_time("11:06 PM", "2:02"))
+test1 = add_time("11:40 AM", "0:25")
+print(test1)
