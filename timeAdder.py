46edb5cd431c826e4c378909ebca87d47d1e21af
Default_String <tikyikhant2334@gmail.com>
2023-04-10 21:21:26 +0630

    new style

--- timeAdder.py
+++ timeAdder.py
@@ -1,4 +1,5 @@
-def add_time(start,end,day=""):
+def add_time(start,duration,day=None):
+    new_time = ""
     date_count = 0
     day_passed = 0
     if day != "" and day != None:
@@ -11,7 +12,7 @@ def add_time(start,end,day=""):
 
     if "AM" not in start.upper() and "PM" not in start.upper():
         time1 = start.strip()
-        time2 = end.strip()
+        time2 = duration.strip()
         newtime1 = time1.split(":")
         newtime2 = time2.split(":")
         res_hr = int(newtime1[0]) + int(newtime2[0])
@@ -29,12 +30,12 @@ def add_time(start,end,day=""):
         """
         if res_min_len == 1:
             res_min = f"0{res_min}"
-        return f"{res_hr}:{res_min}\n"
+        new_time = f"{res_hr}:{res_min}\n"
     elif "AM" in start.upper() or "PM" in start.upper():
         new_list = (start.strip()).split(" ")
         new_str = new_list[0].split(":")
         period = new_list[1]
-        end_str = (end.strip()).split(":")
+        end_str = (duration.strip()).split(":")
         res_min = int(new_str[1]) + int(end_str[1])
         res_hr = int(new_str[0]) + int(end_str[0])
 
@@ -100,13 +101,14 @@ def add_time(start,end,day=""):
             res_min = f"0{res_min}"
         if day != None and day != "":
             if day_passed == 0:
-                return f"{res_hr}:{res_min} {period} {days[date_count]}"
+                new_time = f"{res_hr}:{res_min} {period} {days[date_count]}"
             elif day_passed == 1:
-                return f"{res_hr}:{res_min} {period} {days[date_count]} (next day)\n"
+                new_time = f"{res_hr}:{res_min} {period} {days[date_count]} (next day)\n"
             else:
-                return f"{res_hr}:{res_min} {period} {days[date_count]} ({day_passed} days later)\n"
+                new_time = f"{res_hr}:{res_min} {period} {days[date_count]} ({day_passed} days later)\n"
         else:
-            return f"{res_hr}:{res_min} {period}\n"
+            new_time = f"{res_hr}:{res_min} {period}\n"
+    return new_time
 
 test1 = add_time("11:59 AM","24:05","saturDay")
 print(add_time("11:06 PM", "2:02"))
