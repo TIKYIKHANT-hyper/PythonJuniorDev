bc597885ae7ff432cfbd4a5c585b53846c00cda9
Default_String <tikyikhant2334@gmail.com>
2023-06-15 23:54:40 +0630

    tensorflow

--- newmain.py
+++ newmain.py
@@ -1,1 +1,5 @@
-print("Hello World")
+import tensorflow as tf
+strvar = tf.Variable("when the imposter is sus",tf.string)
+numvar = tf.Variable(69,tf.int8)
+floatvar = tf.Variable(2.5,tf.float16)
+#tf.rank(strvar)
