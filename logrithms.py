30a5b7f1cae872b15d7375bd07c59d760273b233
TIKYIKHANT-hyper <tikyikhant2334@gmail.com>
2023-12-25 00:16:08 +0300

    log

---
+++ logrithms.py
@@ -0,0 +1,6 @@
+import math
+
+var1 = 5**(math.log(10,5))
+var2 = math.log(10,5)
+print(var1)
+print(var2)
---
+++ timetable.py
@@ -0,0 +1,8 @@
+rooms = 5
+days = 7
+timeforeach = days/rooms
+print(timeforeach)
+checktime = 0.4 * 24
+print(checktime)
+leftmin = 0.6 * 60
+print(leftmin)
