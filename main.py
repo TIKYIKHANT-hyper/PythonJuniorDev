4b5abb717aa305dce195733142f46a39d2820b9d
TIKYIKHANT-hyper <tikyikhant2334@gmail.com>
2024-03-31 11:12:50 +0300

    resolved main

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 
 def login(username, password):
+
     driver.refresh()
+    time.sleep(1)
     driver.get("https://internet.msfu.ru/login?")
     time.sleep(2)  # Wait for the page to load
 
@@ -25,12 +27,16 @@ def login(username, password):
 login('mop', "18301919")
 
 # to keep the session alive
+
 while True:
+
     current_url = driver.current_url
-    if current_url != "https://internet.msfu.ru/login?" and current_url != "https://internet.msfu.ru/login":
+    if current_url == "https://internet.msfu.ru/status":
         print("Session is active")
+        driver.refresh()
     else:
         print("Session expired. Re-login required.")
+        driver.refresh()
         login('mop', "18301919")  # Re-login if session expired
 
-    time.sleep(50)  # Check session status every 5 minutes
+    time.sleep(3)  # Check session status every 5 minutes
